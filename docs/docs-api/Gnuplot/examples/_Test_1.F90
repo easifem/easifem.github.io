09974207c561322e03dcbbba111b276d4eae738a
Vikas Sharma <vickysharma0812@gmail.com>
2025-01-02 09:08:03 +0900

    adding gnuplot examples

--- docs/docs-api/Gnuplot/examples/_Test_1.F90
+++ docs/docs-api/Gnuplot/examples/_Test_1.F90
@@ -2,16 +2,26 @@ PROGRAM main
 USE GlobalData
 USE GnuPlot_Class
 
-TYPE(GnuPlot_) :: plot
-REAL(DFP), ALLOCATABLE :: x1(:), y1(:)
+TYPE(Gnuplot_) :: gp
+INTEGER, PARAMETER :: n = 17
+REAL(DFP) :: x(n)
+REAL(DFP) :: y(n)
 
-CALL plot%Initiate()
+x = [-8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8]
+x = ABS(x) + 5
+y = [66, 51, 38, 27, 18, 11, 6, 3, 2, 3, 6, 11, 18, 27, 38, 51, 66]
 
-ALLOCATE (x1(100), y1(100))
+! Annotation: set title, xlabel, ylabel
+CALL gp%title('Example 1. A simple xy plot', '#990011')
 
-CALL RANDOM_NUMBER(x1)
-CALL RANDOM_NUMBER(y1)
+CALL gp%xlabel('my x axis ...', '#99aa33', font_name="Tahoma")
 
-CALL plot%old_plot(x1=x1, y1=y1)
+! call gp%ylabel('my y axis ...')
+
+CALL gp%options('set border lc "#99aa33"; set ylabel "my label..." tc "#99aa33"')
+
+CALL gp%options('set logscale y2')
+
+CALL gp%plot(x, y)
 
 END PROGRAM main
