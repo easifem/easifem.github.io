067509a892667a2fcc636d9c676d1c90c9083449
vickysharma0812 <vickysharma0812@gmail.com>
2025-05-19 00:39:58 +0900

    adding mass matrix example

--- docs/docs-api/MassMatrix/examples/_MassMatrix_test_1.F90
+++ docs/docs-api/MassMatrix/examples/_MassMatrix_test_1.F90
@@ -1,49 +1,70 @@
 PROGRAM main
-USE BaseType, ONLY: ElemShapeData_, QuadraturePoint_, ReferenceLine_
+USE BaseType, ONLY: ElemShapeData_, &
+                    QuadraturePoint_, &
+                    elem => TypeElemNameOpt, &
+                    quadType => TypeQuadratureOpt, &
+                    iptype => TypeInterpolationOpt, &
+                    basisType => TypePolynomialOpt
+
 USE GlobalData, ONLY: DFP, I4B
+
 USE QuadraturePoint_Method, ONLY: QuadPoint_Initiate => Initiate, &
                                   Quad_Size => Size
 
+USE Display_Method, ONLY: Display
+
+USE ElemshapeData_Method, ONLY: LagrangeElemShapeData, &
+                                Elemsd_Allocate => ALLOCATE, &
+                                Elemsd_Set => Set, &
+                                Elemsd_Display => Display
+
 IMPLICIT NONE
 
-TYPE(ElemShapeData_) :: test, trial
+TYPE(ElemShapeData_) :: elemsd, linelemsd
 TYPE(QuadraturePoint_) :: quad
-TYPE(ReferenceLine_) :: refelem
-REAL(DFP), ALLOCATABLE :: mat(:, :), XiJ(:, :)
+REAL(DFP), ALLOCATABLE :: mat(:, :), xij(:, :)
 INTEGER(I4B), PARAMETER :: order = 2, one_i = 1
-INTEGER(I4B) :: nns, nips, tsize
+REAL(DFP), PARAMETER :: refElemCoord(1, 2) = &
+                        RESHAPE([-1.0_DFP, 1.0_DFP], [1, 2])
+INTEGER(I4B) :: nns, nips, tsize, integralOrder
+CHARACTER(LEN=*), PARAMETER :: domainName = "B"
 
 ! Let us now create the physical coordinate of the line element.
-
-XiJ = RESHAPE([-1, 1], [1, 2])
-
-! Now  we create an instance of [[ReferenceLine_]].
-
-refelem = ReferenceLine(nsd=1)
+xij = RESHAPE([-1, 1], [1, 2])
+integralOrder = 2 * order
 
 ! Here, we create the quadrature points.
-
 CALL QuadPoint_Initiate(obj=quad, elemType=elem%line, &
-                        domainName="B", order=integralOrder, &
-                        quadratureType=obj%quadTypeForTime)
+                        domainName=domainName, order=integralOrder, &
+                        quadratureType=quadType%GaussLegendre)
 nips = Quad_Size(quad, 2)
 nns = order + 1
+
+CALL Display(nips, "number of integration points: ")
+CALL Display(nns, "number of nodes in space: ")
 
 CALL Elemsd_Allocate(obj=elemsd, nsd=one_i, xidim=one_i, &
                      nns=nns, nips=nips)
 
-! Initiate an instance of [[ElemshapeData_]]. You can learn more about it from [[ElemshapeData_test]]
+! Initiate an instance of ElemshapeData_ for linear geometry.
+CALL LagrangeElemShapeData(obj=linelemsd, quad=quad, nsd=elemsd%nsd, &
+                           xidim=elemsd%xidim, elemtype=elem%line, &
+                           refelemCoord=refelemCoord, domainName=domainName, &
+                           order=one_i)
 
-CALL Initiate(obj=test, &
-    & quad=quad, &
-    & refelem=refelem, &
-    & ContinuityType=typeH1, &
-    & InterpolType=typeLagrangeInterpolation)
-CALL Set(obj=test, val=xij, N=test%N, dNdXi=test%dNdXi)
+CALL LagrangeElemShapeData(obj=elemsd, quad=quad, nsd=elemsd%nsd, xidim=elemsd%xidim, &
+                           elemType=elem%line, refelemCoord=refelemCoord, &
+                           domainName=domainName, order=order, &
+                           ipType=iptype%GaussLegendreLobatto, &
+                           basisType=basisType%Monomial)
 
-! Let us now create the mass matrix.
+CALL Elemsd_Set(obj=elemsd, val=xij, N=linelemsd%N, dNdXi=linelemsd%dNdXi)
 
-mat = MassMatrix(test=test, trial=test)
-CALL Display(mat, "mat:")
+CALL Elemsd_Display(elemsd, "elemsd:")
+
+! ! Let us now create the mass matrix.
+!
+! mat = MassMatrix(test=test, trial=test)
+! CALL Display(mat, "mat:")
 
 END PROGRAM main
