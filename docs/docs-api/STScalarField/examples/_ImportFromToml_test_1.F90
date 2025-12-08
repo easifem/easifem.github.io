afc2cda2901ef302fdcc810623df1b464c765b8f
Vikas Sharma <vickysharma0812@gmail.com>
2025-12-08 09:04:31 +0900

    Updating STScalarField

---
+++ docs/docs-api/STScalarField/examples/_ImportFromToml_test_1.F90
@@ -0,0 +1,66 @@
+!> author: Vikas Sharma, Ph. D.
+! date: 2025-06-14
+! summary: Testing import from toml
+! In this example we will initiate the fedof outside the ScalarField
+
+PROGRAM main
+USE GlobalData
+USE BaseType, ONLY: TypeQuadratureOpt
+USE AbstractMesh_Class
+USE FEDomain_Class
+USE STScalarField_Class
+USE FEDOF_Class
+USE HDF5File_Class
+USE Test_Method
+USE ExceptionHandler_Class, ONLY: e, EXCEPTION_INFORMATION
+USE TimeFEDOF_Class
+USE TimeOpt_Class, ONLY: TimeOpt_, TypeTimeOpt
+
+IMPLICIT NONE
+
+! Parameters are defined here
+CHARACTER(*), PARAMETER :: engine = "NATIVE_SERIAL"
+CHARACTER(*), PARAMETER :: &
+  meshfilename = "../../FEMesh/examples/meshdata/small_tri3_mesh.h5"
+CHARACTER(*), PARAMETER :: tomlfilename = "./toml/STScalarField.toml"
+INTEGER(I4B), PARAMETER :: nsd = 2
+INTEGER(I4B), PARAMETER :: order = 1
+
+! Variables are defined here
+TYPE(FEDomain_) :: dom
+CLASS(AbstractMesh_), POINTER :: mesh
+TYPE(FEDOF_) :: fedof
+TYPE(STScalarField_) :: obj
+TYPE(TimeFEDOF_) :: timefedof
+TYPE(HDF5File_) :: meshfile
+TYPE(TimeOpt_) :: timeopt
+
+CALL e%setQuietMode(EXCEPTION_INFORMATION, .TRUE.)
+
+CALL meshfile%Initiate(filename=meshfilename, mode="READ")
+CALL meshfile%OPEN()
+CALL dom%Initiate(hdf5=meshfile, group="")
+mesh => dom%GetMeshPointer(dim=nsd)
+
+CALL timeopt%ImportFromToml(tomlName="timeOpt", &
+                            filename=tomlfilename)
+
+CALL obj%ImportFromToml(tomlName="field1", &
+                        filename=tomlfilename, &
+                        fedof=fedof, &
+                        timefedof=timefedof, &
+                        mesh=mesh, &
+                        timeOpt=timeopt &
+                        )
+
+CALL obj%Display("ScalarField obj:")
+
+CALL timefedof%Display("TimeFEDOF obj:")
+
+NULLIFY (mesh)
+CALL obj%DEALLOCATE()
+CALL dom%DEALLOCATE()
+CALL fedof%DEALLOCATE()
+CALL meshfile%DEALLOCATE()
+
+END PROGRAM main
